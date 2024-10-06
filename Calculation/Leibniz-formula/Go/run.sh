bfb7977e8501cfc8d431aaf5feb4c3014001ea38
fukayatti0 <st24162yk@ibaraki.kosen-ac.jp>
2024-10-07 02:10:53 +0900

    all.logにすべての結果をまとめました

--- Calculation/Leibniz-formula/Go/run.sh
+++ Calculation/Leibniz-formula/Go/run.sh
@@ -10,4 +10,4 @@ go build -o Leibniz
 
 execute_time=$(( $(date +%s%2N) - $start_time ))
 
-echo "実行時間: $(echo "scale=2; $execute_time / 100" | bc) 秒" > java.log
+echo "実行時間: $(echo "scale=2; $execute_time / 100" | bc) 秒" > go.log
